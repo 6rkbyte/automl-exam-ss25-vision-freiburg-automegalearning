bea257ed8834e9a6ed7432cd210ba0c609c25e2c
Diogo Cerqueira <digdogopc@gmail.com>
2025-07-29 14:31:36 +0200

    temp. trying to use gpu in kaggle

--- src/automl/automl.py
+++ src/automl/automl.py
@@ -65,23 +65,52 @@ class AutoML:
         input_size = dataset_class.width * dataset_class.height * dataset_class.channels
 
         #model = DummyNN(input_size, dataset_class.num_classes)
-        model = CNN(dataset_class.channels, dataset_class.num_classes)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = CNN(dataset_class.channels, dataset_class.num_classes).to(device)
+        print(next(model.parameters()).device)
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=0.003)
         
         model.train()
+        predictions = [] #!
+        labels = [] #!
+        from time import time
+        t1 = time()
         for epoch in range(5):
+            t11 = time()
             loss_per_batch = []
             for _, (data, target) in enumerate(train_loader):
+                data = data.to(device)
+                target = target.to(device)
+                
                 optimizer.zero_grad()
                 output = model(data)
                 loss = criterion(output, target)
                 loss.backward()
                 optimizer.step()
                 loss_per_batch.append(loss.item())
-            logger.info(f"Epoch {epoch + 1}, Loss: {np.mean(loss_per_batch)}")
+
+                predicted = predicted.to('cpu')
+                target = target.to('cpu')
+                
+                predictions.append(predicted.numpy()) #! accuracy
+                labels.append(target.numpy())         #! accuracy
+            predictions = np.concatenate(predictions)
+            labels = np.concatenate(labels)
+            
+            t12 = time()
+            logger.info(f"Epoch {epoch + 1}, Loss: {np.mean(loss_per_batch)}, Time: {t12 - t11}")
+			#!  accuracy & time
+            from sklearn.metrics import accuracy_score
+            if not np.isnan(labels).any():
+                acc = accuracy_score(labels, predictions)
+                logger.info(f"Accuracy on train set: {acc}\n")
+                labels, predictions = [], []
+            #!/ accuracy & time
         model.eval()
         self._model = model
+        t2 = time()
+        print(f'took {t2-t1} seconds')
 
         return self
 
@@ -98,10 +127,17 @@ class AutoML:
         predictions = []
         labels = []
         self._model.eval()
+        
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
         with torch.no_grad():
             for data, target in data_loader:
                 output = self._model(data)
                 predicted = torch.argmax(output, 1)
+                
+                predicted = predicted.to('cpu')
+                target = target.to('cpu')
+                
                 labels.append(target.numpy())
                 predictions.append(predicted.numpy())
         predictions = np.concatenate(predictions)
